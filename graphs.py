6316ddd44ed120a62b2e550bc88d9cbfb77bf1a6
kubakalisciak <168641088+kubakalisciak@users.noreply.github.com>
2025-12-26 21:03:41 +0100

    Add graph of daily average delays

--- graphs.py
+++ graphs.py
@@ -1,24 +1,29 @@
 import matplotlib.pyplot as plt
 import os, csv
 
+dir_name = 'output'
 
-def graph_average_delays_by_line():
-    # read data from csv file
-    dir_name = 'output'
+def read_data_from_file():
     filename = os.path.join(dir_name, 'output.csv')
-    fieldnames = ['line', 'calculated_delay', 'date', 'time', 'vehicle_number']
-    count = {}
-    total_delay = {}
+    output = []
     with open(filename, 'r') as file:
         reader = csv.DictReader(file)
         for row in reader:
-            line = row['line']
-            try:
-                count[line] += 1
-                total_delay[line] += int(row['calculated_delay'])
-            except KeyError:
-                count[line] = 1
-                total_delay[line] = int(row['calculated_delay'])
+            output.append(row)
+
+    return output
+
+def graph_average_delays_by_line():
+    count, total_delay = {}, {}
+    data = read_data_from_file()
+    for row in data:
+        line = row['line']
+        try:
+            count[line] += 1
+            total_delay[line] += int(row['calculated_delay'])
+        except KeyError:
+            count[line] = 1
+            total_delay[line] = int(row['calculated_delay'])
 
     # compute average delays
     avg_delays = {}
@@ -40,15 +45,53 @@ def graph_average_delays_by_line():
     axis.set_title('Average Delay by Line')
     plt.xticks(rotation=90)
     plt.tight_layout()
-    axis.grid(axis='y', linestyle='--', alpha=0.5)
-    axis.grid(axis='x', linestyle='--', alpha=0.3)
+    axis.grid(axis='x', linestyle='-', alpha=0.3)
 
     plt.savefig(os.path.join(dir_name, 'avg_delay__line.png'))
     plt.close()
 
 
+def graph_average_delays_by_day():
+    data = read_data_from_file()
+    count, total_delay = {}, {}
+    for row in data:
+        date = row['date']
+        try:
+            count[date] += 1
+            total_delay[date] += int(row['calculated_delay'])
+        except KeyError:
+            count[date] = 1
+            total_delay[date] = int(row['calculated_delay'])
+
+    # compute average delays
+    avg_delays = {}
+    for i in count.keys():
+        avg_delays[i] = total_delay[i] / count[i]
+    avg_delays = dict(sorted(avg_delays.items()))
+
+    # plot the graph
+    sorted_items = sorted(avg_delays.items())
+    labels = [str(k) for k, v in sorted_items]
+    values = [v for k, v in sorted_items]
+
+    fig = plt.figure()
+    axis = fig.add_subplot(111)
+
+    axis.bar(labels, values)
+    axis.set_xlabel('Line')
+    axis.set_ylabel('Average Delay')
+    axis.set_title('Average Delay by Day')
+    plt.xticks(rotation=90)
+    plt.tight_layout()
+    axis.grid(axis='x', linestyle='-', alpha=0.3)
+
+    plt.savefig(os.path.join(dir_name, 'avg_delay__day.png'))
+    plt.close()
+
+
+
 def main():
-    graph_average_delays_by_line()
+    graph_average_delays_by_day()
 
 
 if __name__ == "__main__":
